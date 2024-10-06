1e638d0aad25db7a720c3729c8fd84af050c5583
Pedro Marques <hey@pedromarques.io>
2024-10-06 17:59:47 +0200

    Initial Commit

---
+++ todaylastyear/todaylastyearApp.swift
@@ -0,0 +1,17 @@
+//
+//  todaylastyearApp.swift
+//  todaylastyear
+//
+//  Created by Pedro Marques on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct todaylastyearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
